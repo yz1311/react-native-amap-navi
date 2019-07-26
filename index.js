403b68f50556df401a86fd5a377c86008e5de5c6
yangzhao <yz1311@foxmail.com>
2019-07-26 14:42:08 +0800

    fix

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import { NativeModules } from 'react-native';
 
 const { RNReactNativeAmapNavi } = NativeModules;
 
+export const AmapNaviType = {
+    DRIVER: 0,
+    WALK: 1,
+    RIDE: 2
+}
+
 export default {
     showRouteActivity (points,navType) {
         return RNReactNativeAmapNavi.showRouteActivity(points,navType);
     },
-    AmapNaviType: {
-        DRIVER: 0,
-        WALK: 1,
-        RIDE: 2
-    }
 };
